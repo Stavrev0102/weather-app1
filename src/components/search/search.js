a1e98fd2e9fdac62081151390bc38d0d6cbfd0e0
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2024-03-16 21:58:08 +0200

    implement search page

---
+++ src/components/search/search.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
+
+const Search = ({ onSearchChange}) => {
+    // eslint-disable-next-line no-unused-vars
+    const [search,setSearch] = useState(null);
+    const loadOptions = (inputValue) => {
+        
+    }
+
+    const handleOnChange = (searchData) => {
+         setSearch(searchData)
+         onSearchChange(searchData)
+    }
+
+    return (
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
+
+}
+export default Search
