ccda755a836be608524a38722505cbda77b15c65
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2024-03-16 22:26:20 +0200

    Implement search functionality

---
+++ src/api.js
@@ -0,0 +1,10 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ab94ccd492mshb88218e6c502ba8p15dec8jsn9d660fd00f8d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities'
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,12 +1,29 @@
 import { useState } from "react"
 import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange}) => {
     // eslint-disable-next-line no-unused-vars
     const [search,setSearch] = useState(null);
-    const loadOptions = (inputValue) => {
+    const loadOptions = async(inputValue) => {
+            return fetch(
+              `${GEO_API_URL}?minPopulation=1000000&namePrefix=${inputValue}`,
+              geoApiOptions
+            )
+              .then((res) => res.json())
+              .then((res) => {
+                return {
+                  options: res.data.map((city) => {
+                    return {
+                      value: `${city.latitude} ${city.longitude}`,
+                      label: `${city.name} , ${city.countryCode}`,
+                    };
+                  }),
+                };
+              })
+              .catch((err) => console.error(err));
+        }
         
-    }
 
     const handleOnChange = (searchData) => {
          setSearch(searchData)
