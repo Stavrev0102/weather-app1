76d0e53b2b6e1ce8ff218f4982244c5a76c32b85
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2024-03-20 19:55:46 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL } from './api';
 
 function App() {
 
     const handleOnSearchChange = (searchData) => {
-      console.log(searchData)
+      const [lat,long] = searchData.value.split(" ");
+      const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid={API key}`)
     }
 
   return (
