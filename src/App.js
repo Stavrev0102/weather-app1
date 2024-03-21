e2e99f9d5d37e1320f7617551a4a79f67a0872dc
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2024-03-21 20:39:24 +0200

    Add api keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
-import { WEATHER_API_URL } from './api';
+import { WEATHER_API_URL , WEATHER_API_KEY } from './api';
 
 function App() {
 
     const handleOnSearchChange = (searchData) => {
       const [lat,long] = searchData.value.split(" ");
-      const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid={API key}`)
+      const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid=${WEATHER_API_KEY}`)
     }
 
   return (
