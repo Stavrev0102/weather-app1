5e4e083eaf5bd899e737fb0451b9877a82c14811
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2024-03-25 20:12:02 +0200

    add 4 tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,12 +1,41 @@
-const { test , expect } = require('@playwright/test');
+/* eslint-disable no-unused-vars */
+const { test, expect } = require('@playwright/test');
 
 const baseUrl = 'http://localhost:3000';
-const navBarElement = 'nav.navbar';
+const searchBarSelector = '.css-13cymwt-control'; // Assuming this is the correct selector for the search bar
+const dropDownButtonSelector = '.css-1xc3v61-indicatorContainer'
+const dropDownListSelector = '.css-1nmdiq5-menu'
 
-test('verify if all-links page is visible', async({ page }) => {
+test('verify if search bar is visible', async({ page }) => {
     await page.goto(baseUrl);
-    await page.waitForSelector(navBarElement);
-    const allBooksLink = await page.$('a[href="/catalog"]');
-    const isLinkVisible = await allBooksLink.isVisible();
-    expect(isLinkVisible).toBe(true)
+    await page.waitForSelector(searchBarSelector);
+    const searchBar = await page.$(searchBarSelector);
+    const isSearchBarVisible = await searchBar.isVisible();
+    expect(isSearchBarVisible).toBe(true);
 });
+
+
+test('verify if dropDownButton is available', async({ page }) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector(searchBarSelector);
+    const dropDownButton = await page.$(dropDownButtonSelector);
+    const isDropDownButtonAvailable = await dropDownButton.isVisible();
+    expect(isDropDownButtonAvailable).toBe(true);
+});
+test('verify if dropDownList is available', async({ page }) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector(searchBarSelector);
+    await page.click(dropDownButtonSelector);
+    const dropDownList = await page.$(dropDownListSelector)
+    const isListAvailable = await dropDownList.isVisible()
+    expect(isListAvailable).toBe(true);
+});
+test('Search City London', async({page}) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector(searchBarSelector);
+    await page.fill('#react-select-3-input', 'London');
+    await page.click('.css-1nmdiq5-menu:nth-child(1)');
+    const weatherElement = await page.$('weather');
+    const isWeatherInfoAvailable = await weatherElement.isVisible()
+    expect(isWeatherInfoAvailable).toBe(true)
+})
